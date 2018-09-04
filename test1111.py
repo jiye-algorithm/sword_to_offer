055c769744c40e2dd37f1f19df769335a7c98b56
woxinxie1234 <woxinxie1234@163.com>
2018-09-04 20:12:16 +0800

    Update test1111.py

--- test1111.py
+++ test1111.py
@@ -1,50 +1,30 @@
+my_p = list(map(int, input().strip().split()))
+my_q = list(map(int, input().strip().split()))
 
-line ={}
-chaxun_=[]
-k = 0
-dont_move_key = 0
-not_move =  0
-flag = False
-n,m = map(int,input().split(" "))
-for i in range(n):
-    li = input().split(" ")
-    line[li[0]] = [int(li[2]),-1,li[4]]
-for index in range(m):
-    chaxun_.append(input().split(" "))
-for key in list(line.keys()):
-    k = key
-    if line.get(line[key][-1],0) ==0:
-        not_move = line[key][-1]
-        dont_move_key = key
-        flag = True
-if not flag :
-    not_move = line[k][-1]
-    dont_move_key = k
-for i in range(len(line)):
-    for key in line:
-        if line[key][-1] == dont_move_key :
-            line[key] = [line[key][0] - line[dont_move_key][0], line[key][1] *line[key][1],line[dont_move_key][-1]]
-            dont_move_key = key
-for chaxun in chaxun_:
-    if chaxun[2] == not_move:
-        ll = 0
-        kk = 1
-    else:
-        ll =  line[chaxun[2]][0]
-        kk =  line[chaxun[2]][1]
-    if chaxun[0] == not_move:
-        l2 = 0
-        k2 = 1
-    else:
-        l2 = line[chaxun[0]][0]
-        k2 = line[chaxun[0]][1]
-    result = l2 -  ll
-    if (k2 * kk)<0 :
-      print('cannot_answer')
-    else:
-      print(result)
+from collections import Counter
+from math import log2
 
+counter_p = Counter(my_p)
+counter_q = Counter(my_q)
 
+count_p = 0
+count_q = 0
+for item in counter_p.items():
+    count_p += item[1]
+    count_q += counter_q.get(item[0])
 
+prob_p = {}
+prob_q = {}
+for item in counter_p.items():
+    prob_p[item[0]] = item[1] / count_p
+    prob_q[item[0]] = counter_q.get(item[0]) / count_q
 
+for item in counter_p.items():
+    prob_p[item[0]] = item[1] / float(count_p)
+    prob_q[item[0]] = counter_q.get(item[0]) / float(count_q)
 
+ans = 0
+for item in counter_p.items():
+    ans += prob_p[item[0]] * log2(prob_p[item[0]] / prob_q[item[0]])
+
+print("{:.2f}".format(ans))
