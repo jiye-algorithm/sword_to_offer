3e9d989e7f42f249805de2c8a130a892e72271b5
jiye <woxinxie1234@163.com>
2018-09-07 09:34:22 +0800

    面试

--- test4.py
+++ test4.py
@@ -1,43 +1,6 @@
-my_input = str(input().strip().split())
-my_input_len = len(input().strip().split())
+n, k = map(int, input().strip().split())
 
+lists = map(int, input().strip().split())
 
-def yunsuan(a,b,c):
-    if c == "+":
-        return a+b
-    if c == "-":
-        return b-a
-    if c == "*":
-        return a*b
-    if c == "/":
-        return b/a
-
-def compare(operator_left, operator_right):
-    if operator_left in '+-' and operator_right in '*/':
-        return True
-    return False
-
-
-operator_stack = list()
-operand_stack = list()
-ans = 0
-for ind, num in enumerate(my_input):
-    if num == my_input[-1]:
-        ans = yunsuan(operand_stack[-1], int(num), operator_stack[-1])
-    else:
-        if num in '+-*/':
-            while len(operator_stack) != 0 and compare(operator_stack[-1], num):
-                temp = yunsuan(operand_stack[-2], operand_stack[-1], operator_stack[-1])
-                operand_stack = operand_stack[: -2]
-                operator_stack = operator_stack[: -1]
-                operand_stack.append(temp)
-        else:
-            operand_stack.append(int(num))
-
-print(ans)
-
-
-
-
-
-
+for i in n:
+    for j in range(0, n - i)
