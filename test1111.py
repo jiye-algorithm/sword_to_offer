0207ed5856908a0a5c7184af5c51fca7ef827091
woxinxie1234 <woxinxie1234@163.com>
2018-09-08 16:43:03 +0800

    Update test1111.py

--- test1111.py
+++ test1111.py
@@ -1,10 +1,21 @@
-t = int(input().strip())
-
-
-for _ in range(t):
-    n, m = [int(a) for a in input().strip().split()]
-    ans = (n-2)*(m-2)
-    if ans < 0:
-        print(-ans)
+line = list(input().strip("\n"))
+num= 1
+pre= 0
+for i in range(len(line) -1):
+    if line[i] == line[i+1]:
+        if (line[i] == line[0] and line[i+1] == line[-1]) or (line[0] == line[-1]):
+            if pre <num:
+                pre = num
+            num =1
+            continue
+        else:
+            part1 = line[:i+1]
+            reversed(part1)
+            part2 = line[i+1:]
+            reversed(part2)
+            line = part1 + part2
+            num += 1
     else:
-        print(ans)
+        num +=1
+
+print(num)
