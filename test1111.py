7be841dfcae2188a8718c0d8cb37ec1a138dab40
woxinxie1234 <woxinxie1234@163.com>
2018-09-09 20:56:21 +0800

    Update test1111.py

--- test1111.py
+++ test1111.py
@@ -1,37 +1,65 @@
-import java.util.Scanner;
+#!/bin/python
+# -*- coding: utf8 -*-
+import sys
+import os
+import re
 
-public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        int [][]arr = new int [n][3];
-        for(int i = 0;i<n;i++){
-        	String []str = sc.nextLine().split(" ");
-        	arr[i][0] = Integer.valueOf(str[0]);
-        	arr[i][1] = Integer.valueOf(str[1]);
-        	arr[i][2] = Integer.valueOf(str[2]);
-        }
-        int count=0;
-        for(int i = 0;i<n;i++){
-        	boolean flag = true;
-        	for(int j = 0;j<n;j++){
-        		if(j!=i&&compare(arr,i,j)){
-        			flag = false;
-        			break;
-        		}
-        	}
-        	if(flag==false){
-        		count++;
-        	}
-        }
-        System.out.println(count);
-        
-    }
-    public static boolean compare(int [][]arr, int a,int b){
-    	if(arr[b][0]>arr[a][0]&&arr[b][1]>arr[a][1]&&arr[b][2]>arr[a][2])
-    		return true;
-    	return false;
-    }
-}
+# 请完成下面这个函数，实现题目要求的功能
+# 当然，你也可以不按照下面这个模板来作答，完全按照自己的想法来 ^-^
+# ******************************开始写代码******************************
+
+
+def rrr(s, t):
+    r = dict()
+    for _t_index, _t in enumerate(t):
+        has_r = r.get(_t, "0")
+        if has_r == "0":
+            r[_t] = s[_t_index]
+        else:
+            if has_r != s[_t_index]:
+                return 0
+        pass
+
+    r = dict()
+    for _s_index, _s in enumerate(s):
+        has_r = r.get(_s, "0")
+        if has_r == "0":
+            r[_s] = t[_s_index]
+        else:
+            if has_r != t[_s_index]:
+                return 0
+        pass
+    return 1
+    pass
+
+
+def solve(S, T):
+
+    result_count = 0
+    for i in range(len(S) - len(T) + 1):
+        _S = S[i: i + len(T)]
+        count = rrr(_S, T)
+        result_count += count
+        pass
+
+    return result_count
+
+# ******************************结束写代码******************************
+
+
+try:
+    _S = input()
+    # _S = "ababcb"
+except:
+    _S = None
+
+try:
+    _T = input()
+    # _T = "ded"
+except:
+    _T = None
+
+res = solve(_S, _T)
+
+print(str(res) + "\n")
