5b1223bc3b76da3042c7b89abf8ee9da09f8fbfc
woxinxie1234 <woxinxie1234@163.com>
2018-09-09 10:42:29 +0800

    Update test1111.py

--- test1111.py
+++ test1111.py
@@ -3,21 +3,33 @@ M = int(input().strip())
 
 my_input = list(map(int, input().strip().split()))
 
-i = 0
 my_map = {}
 for i in set(my_input):
-    my_map[i] = []
+    my_map[i] = set()
 
+i = 0
 while i < len(my_input):
     start, end = my_input[i : i + 2]
-    my_map[end].append(start)
+    my_map[end].add(start)
     for it in my_map[start]:
-        my_map[end].append(it)
+        my_map[end].add(it)
     my_map[end] = set(my_map[end])
-    
+    i += 2
+
+ans = 0
 for key, value in my_map.items():
-    if len(my_map[key]) == len(my_map):
-        print(key)
+    count = 0
+    for key2, value2 in my_map.items():
+        if key == key2:
+            continue
+        if key in value2:
+            count += 1
+    if count == N - 1:
+        ans = key
         break
-    
+
+
+print(ans)
+
+
 
