4904a6a37e10ec488b31a846aad07885ec1ac99f
woxinxie1234 <woxinxie1234@163.com>
2018-09-09 11:12:30 +0800

    Update test1111.py

--- test1111.py
+++ test1111.py
@@ -1,24 +1,30 @@
-M = int(input().strip())
-input_matrix = []
-for _ in range(M):
-    input_matrix.append(list(map(int, input().strip().split())))
+num = int(input().strip())
+matrix = []
+for _ in range(num):
+    per = list(map(int, input().strip().split()))
+    matrix.append(per)
 
-flag = [[0] * M for i in range(M)]
-def find(matrix, i, j, flag):
-    if 0<= i < M and 0<= j < M:
-        if matrix[i][j] and not flag[i][j]:
+flag = [[0] * num for _ in range(num)]
+
+
+def search(content, i, j, flag):
+    if 0<= i < num and 0<= j < num:
+        if content[i][j] and not flag[i][j]:
             flag[i][j] = True
-            find(matrix, i-1, j, flag)
-            find(matrix, i+1, j, flag)
-            find(matrix, i, j-1, flag)
-            find(matrix, i, j+1, flag)
+            search(content, i-1, j, flag)
+            search(content, i+1, j, flag)
+            search(content, i, j-1, flag)
+            search(content, i, j+1, flag)
             return True
     return False
 
-count = 0
-for i in range(M):
-    for j in range(M):
-        if find(input_matrix, i, j, flag):
-            count += 1
-print(count)
+def num_of_connect(content):
+    count = 0
+    for i in range(num):
+        for j in range(num):
+            if search(content,i,j, flag):
+                count += 1
+    print(count)
+
+num_of_connect(matrix)
 
