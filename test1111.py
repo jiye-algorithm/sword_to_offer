c13bd869b172aa7f26765f824f1e95cc53af643c
woxinxie1234 <woxinxie1234@163.com>
2018-09-09 11:11:30 +0800

    Update test1111.py

--- test1111.py
+++ test1111.py
@@ -1,21 +1,24 @@
-line = input().strip()
+M = int(input().strip())
+input_matrix = []
+for _ in range(M):
+    input_matrix.append(list(map(int, input().strip().split())))
 
-left = 0
-right = 0
-ans = 0
-visited = [0] * 256 
-while left < len(line):
-    if right < len(line) and visited[ord(line[right])] == 0:
-        visited[ord(line[right])] += 1
-        right += 1
-    else:
-        visited[ord(line[left])] -= 1
-        left += 1
+flag = [[0] * M for i in range(M)]
+def find(matrix, i, j, flag):
+    if 0<= i < M and 0<= j < M:
+        if matrix[i][j] and not flag[i][j]:
+            flag[i][j] = True
+            find(matrix, i-1, j, flag)
+            find(matrix, i+1, j, flag)
+            find(matrix, i, j-1, flag)
+            find(matrix, i, j+1, flag)
+            return True
+    return False
 
-    asn = max(ans, right - left)
+count = 0
+for i in range(M):
+    for j in range(M):
+        if find(input_matrix, i, j, flag):
+            count += 1
+print(count)
 
-print(ans)
-
-
-    
-    
