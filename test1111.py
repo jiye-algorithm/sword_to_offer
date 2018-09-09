cdb055247a91be2c8edcd28d57c078b11dd4ff37
woxinxie1234 <woxinxie1234@163.com>
2018-09-09 11:26:07 +0800

    Update test1111.py

--- test1111.py
+++ test1111.py
@@ -1,30 +1,27 @@
-num = int(input().strip())
-matrix = []
-for _ in range(num):
-    per = list(map(int, input().strip().split()))
-    matrix.append(per)
+def forward(follow, mid_var, ans, count, i, length):
+    if i ==length and count == 0:
+        ans.append(mid_var)
+        return ans
+    if (count== 0 and i < length) or (count > 0 and i == length):
+        return ans
+    flag = True
+    num = 0
+    while i < length and flag:
+        if num == 0 and follow[i] == "0":
+            flag = False
+        num = num * 10 + ord(follow[i]) - ord('0')
+        i += 1
+        if num < 256:
+            record = ""
+            if num == 1:
+                record = mid_var + str(num)
+            else:
+                record = mid_var + str(num) + "."
+                forward(follow, record, ans, count-1, i, length)
+    return ans
 
-flag = [[0] * num for _ in range(num)]
-
-
-def search(content, i, j, flag):
-    if 0<= i < num and 0<= j < num:
-        if content[i][j] and not flag[i][j]:
-            flag[i][j] = True
-            search(content, i-1, j, flag)
-            search(content, i+1, j, flag)
-            search(content, i, j-1, flag)
-            search(content, i, j+1, flag)
-            return True
-    return False
-
-def num_of_connect(content):
-    count = 0
-    for i in range(num):
-        for j in range(num):
-            if search(content,i,j, flag):
-                count += 1
-    print(count)
-
-num_of_connect(matrix)
-
+my_input = input().strip()
+ans = []
+mid_var = ""
+ans = forward(my_input, mid_var, ans,4, 0,  len(my_input))
+print(len(ans))
