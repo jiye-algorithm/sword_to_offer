85edf3dc617f0da39003819c51b4240da98e2201
jiye <woxinxie1234@163.com>
2018-09-13 00:51:48 +0800

    KMP

--- python代码/KMP.py
+++ python代码/KMP.py
@@ -1,13 +1,18 @@
 # KMP
+'''
+http://www.ruanyifeng.com/blog/2013/05/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm.html
+https://blog.csdn.net/handsomekang/article/details/40978213
+'''
 def kmp_match(s, p):
-    m = len(s);
+    m = len(s)
     n = len(p)
     cur = 0  # 起始指针cur
     table = partial_table(p)
     while cur <= m - n:
         for i in range(n):
             if s[i + cur] != p[i]:
-                cur += max(i - table[i - 1], 1)  # 有了部分匹配表,我们不只是单纯的1位1位往右移,可以一次移动多位
+                # 移动位数 = 已匹配的字符数 - 对应的部分匹配值，每次最少一定一位
+                cur += max(i - table[i - 1], 1)
                 break
         else:
             return True
@@ -17,12 +22,18 @@ def kmp_match(s, p):
 # 部分匹配表
 def partial_table(p):
     '''partial_table("ABCDABD") -> [0, 0, 0, 0, 1, 2, 0]'''
+    # 部分串的前缀
     prefix = set()
+    # 部分串的后缀
     postfix = set()
+    # 模式串，也就是此时前缀和后缀交集的长度
     ret = [0]
     for i in range(1, len(p)):
+        # 加入新前缀 abc  -> [a, ab]
         prefix.add(p[:i])
+        # 加入所有可能的后缀 abc  -> [bc, c]
         postfix = {p[j:i + 1] for j in range(1, i + 1)}
+        # 前缀和后缀的交集
         ret.append(len((prefix & postfix or {''}).pop()))
     return ret
 
--- test2.py
+++ test2.py
@@ -1,15 +1,9 @@
-dp = [0 for _ in range(1000001)]
-
-dp[1] = 1
-dp[2] = 2
-for i in range(3, 1000001):
-    if (i % 2 == 1):
-        dp[i] = dp[i - 1]
+def fun(i):
+    if i > 1:
+        return i * fun(i - 1)
     else:
-        dp[i] = dp[i - 2] * 2 + dp[i // 2]
+        return 1
 
-M = int(input().strip())
-for i in range(M):
-    n = int(input().strip())
-    print(dp[n])
-    pass
+if __name__ == '__main__':
+
+    print(fun(6))
